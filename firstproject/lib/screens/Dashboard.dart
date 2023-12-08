9554802ced1db2b5a5db9f470cf0d30f1abb88bc
komang <yudamuliawan00@gmail.com>
2023-12-08 16:14:28 +0800

    Desain Container

--- firstproject/lib/screens/Dashboard.dart
+++ firstproject/lib/screens/Dashboard.dart
@@ -8,16 +8,33 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Center(
-        child: IconButton(
-          icon: Icon(FontAwesomeIcons.angular),
-          onPressed: () {},
-          iconSize: 100,
-          color: Colors.lightBlue,
-          splashColor: Colors.white,
-          highlightColor: Colors.red,
+      backgroundColor: Colors.white,
+      body: Container(
+        width: 350.0,
+        height: 250.0,
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.all(20.0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: Colors.blueGrey,
+          // borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: Colors.grey, width: 6.0),
+          shape: BoxShape.circle,
+          image: DecorationImage(image: AssetImage("images/logo.png")),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade500,
+              blurRadius: 7,
+              spreadRadius: 5,
+              offset: Offset(4,4),
+            ),
+          ],
         ),
+
+      // child: Text(
+      //   "Boring",
+      //   style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+      // ),
       ),
     );
   }
