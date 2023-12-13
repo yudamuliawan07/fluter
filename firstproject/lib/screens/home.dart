28723e62f04291879e7b9442f26f84218c069d7a
komang <yudamuliawan00@gmail.com>
2023-12-13 15:01:35 +0800

    ListView dan ListTile

--- firstproject/lib/screens/home.dart
+++ firstproject/lib/screens/home.dart
@@ -6,72 +6,40 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      appBar: AppBar(
-        leading:  IconButton(icon:   Icon(Icons.menu), onPressed: (){},),
-        title: Text("Home"),
-        actions: [
-          IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){},),
-          IconButton(icon: Icon(Icons.search), onPressed: (){},),
-          IconButton(icon: Icon(Icons.menu_book), onPressed: (){},),
-        ],
-        elevation: 0,
-        // titleSpacing: 40.0,
-        backgroundColor: Colors.grey,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
-        ),
-
-      ),
       body: Container(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(child: Image(image: AssetImage("images/logo.png"), width: 150,)),
-                  Expanded(flex: 2, child: Image(image: AssetImage("images/logo.png"), width: 150,)),
-                  Expanded(flex: 4,child: Image(image: AssetImage("images/logo.png"), width: 150,)),
-
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.star),
-                  Icon(Icons.star),
-                  Icon(Icons.star_border),
-                  Icon(Icons.star_border),
-                  Icon(Icons.star_border),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-                children: [
-                  Column(
-                      children: [
-                        Icon(Icons.phone, size: 35.0,),
-                        Text("Phone")
-                      ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.alt_route, size: 35.0,),
-                      Text("Route")
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.share, size: 35.0,),
-                      Text("Share")
-                    ],
-                  ),
-
-                ],
-              ),
-            ],
-          ),
+        child: ListView(
+          itemExtent: 100.0,
+          reverse: false,
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          children: [
+            ListTile(
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.alarm_on_sharp),backgroundColor: Colors.black12,),
+              title: Text("Sales"),
+              subtitle: Text("Sales of the waek"),
+              trailing: Text("350"),
+              onTap: (){},
+              tileColor: Colors.grey.shade200,
+            ),
+            ListTile(
+                leading: Icon(Icons.supervised_user_circle_outlined),
+              title: Text("Customers"),
+              subtitle: Text("Total Customer"),
+              trailing: Text("20"),
+            ),
+            ListTile(
+              leading: Icon(Icons.alarm_on_sharp),
+              title: Text("Profit"),
+              subtitle: Text("Profit of the waek"),
+              trailing: Text("20000"),
+            ),
+          ],
+        ),
       ),
+
     );
   }
 }
