2d181508302ac9102b6514ef1b7b9cb563d9f862
komang <yudamuliawan00@gmail.com>
2023-12-14 10:41:25 +0800

    Statefull 1 Liked Button

--- firstproject/lib/main.dart
+++ firstproject/lib/main.dart
@@ -1,20 +1,50 @@
-import 'screens/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(CTStatelessClass());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class CTStatelessClass extends StatelessWidget {
+  const CTStatelessClass({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "My Flutter App",
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: CTStatefullClass(),
+      ),
+    );
+  }
+}
+
+class CTStatefullClass extends StatefulWidget {
+  const CTStatefullClass({super.key});
+
+  @override
+  State<CTStatefullClass> createState() => _CTStatefullClassState();
+}
+
+class _CTStatefullClassState extends State<CTStatefullClass> {
+  bool liked = false;
+
+  @override
+
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          title: Text("Text"),
+          trailing: IconButton(
+            icon: liked? (Icon(Icons.favorite_outline)) : (Icon(Icons.favorite)),
+            onPressed: () {
+              setState(() {
+                liked = !liked;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
